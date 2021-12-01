7cefc48995342087ee557c2f30960a2aa2acc5cd
Ghadeerrr <91834143+Ghadeerrr@users.noreply.github.com>
2021-12-01 14:19:28 +0300

    uploed Patient folder

--- Hospital/demo/src/main/java/com/example/demo/patients/PatientController.java
+++ Hospital/demo/src/main/java/com/example/demo/patients/PatientController.java
@@ -1,22 +1,52 @@
 package com.example.demo.patients;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path= "patients")
 public class PatientController {
 
-    private final PatientService patientservice;
+    private final PatientService patientService;
 
 
     @Autowired
-    public PatientController(PatientService patientservice) {
-        this.patientservice = patientservice;
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
     }
 
+    @GetMapping
+    public List<Patient> getPatientS(){
+        return patientService.getPatients();
 
+    }
 
+    @GetMapping("/{id}")
+    public Patient getPatient(@PathVariable String id){
+        return patientService.getPatient(id);
+
+    }
+
+    @PostMapping
+    public Patient createPatient(@RequestBody Patient patient){
+        return patientService.createPatient(patient);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePatient(@PathVariable String id){
+        patientService.deletePatient(id);
+
+    }
+
+    @PutMapping("/{id}")
+    public Patient updatePatient(@PathVariable String id, @RequestBody Patient data){
+       return patientService.updatePatient(id, data);
+    }
 
 }
+
+
+
+
