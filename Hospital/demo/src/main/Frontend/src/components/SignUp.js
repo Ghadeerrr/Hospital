f81308f4ef7d734e9fc5f256b51f1102e6da3261
Amal <amalgassam@hotmail.com>
2021-12-05 14:32:18 +0300

    update 2:30pm:

--- Hospital/demo/src/main/Frontend/src/components/SignUp.js
+++ Hospital/demo/src/main/Frontend/src/components/SignUp.js
@@ -1,94 +1,202 @@
-// import { useDispatch, useSelector } from "react-redux";
-// import { setLoginDetails } from "../reducers/Login/login";
-// import { setWishList } from "../reducers/wishList/wishList";
-// import { setCartUsers } from "../reducers/cart/cart";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
-// import { useNavigate } from "react-router-dom";
-// import { setUserName, setId } from "../reducers/Login/login";
 
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import CloseIcon from "@material-ui/icons/Close";
 import "./Login.css";
 
 function SignUp() {
-  // const navigate = useNavigate();
-  // const state = useSelector((state) => {
-  //   return {
-  //     wishListUsers: state.wishList.wishListUsers,
-  //     loginDetails: state.loginDetails.loginDetails,
-  //     cartUsers: state.Cart.cartUsers,
-  //   };
-  // });
-  // console.log(state.loginDetails);
-  // const dispatch = useDispatch();
-  // let firstName;
-  // let lastName;
-  // let userName;
-  // let email;
-  // let password;
+  const [patients, setpatients] = useState();
+  const [doctors, setdoctors] = useState();
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const [phoneNumber, setPhoneNumber] = useState();
+  const [password, setPassword] = useState();
+  const [category, setCategory] = useState();
+  const [bloodType, setBloodType] = useState();
+  const [age, setAge] = useState();
+  const [gender, setGender] = useState();
+  const [department, setDepartment] = useState();
+  
 
-  // const changeFirstName = (e) => {
-  //   firstName = e.target.value;
-  // };
-  // const changeLastName = (e) => {
-  //   lastName = e.target.value;
-  // };
-  // const changeUserName = (e) => {
-  //   userName = e.target.value;
-  // };
-  // const changeEmail = (e) => {
-  //   email = e.target.value;
-  // };
-  // const changePassword = (e) => {
-  //   password = e.target.value;
-  // };
-  // const Cheack = (e) => {
-  //   e.preventDefault();
-  //   console.log("check");
+  const navigate = useNavigate();
 
-  //   let x = 0;
-  //   let y = 0;
-  //   for (let i = 0; i < state.loginDetails.length; i++) {
-  //     if (state.loginDetails[i].email == email) {
-  //       x = 1;
-  //       alert("The email is taken");
-  //       break;
-  //     }
-  //     if (state.loginDetails[i].username == userName) {
-  //       y = 1;
-  //       alert("The username is taken");
-  //       break;
-  //     }
-  //   }
-  //   if (x == 0 && y == 0) {
-  //     let newUser = {
-  //       id: state.loginDetails.length + 1,
-  //       email: email,
-  //       username: userName,
-  //       password: password,
-  //       name: { firstname: firstName, lastname: lastName },
-  //     };
-  //     let arr = state.loginDetails.slice();
-  //     console.log(arr);
-  //     arr.push(newUser);
-  //     console.log(arr);
-  //     const action = setLoginDetails(arr);
-  //     dispatch(action);
-  //     let arrW = state.wishListUsers.slice();
-  //     arrW.push({ id: state.loginDetails.length + 1, wishList: [] });
-  //     const action2 = setWishList(arrW);
-  //     dispatch(action2);
-  //     const action4 = setUserName(userName);
-  //     dispatch(action4);
-  //     const action5 = setId(state.loginDetails.length + 1);
-  //     dispatch(action5);
-  //     let arrc = state.cartUsers.slice();
-  //     arrc.push({ id: state.loginDetails.length + 1, cart: [] });
-  //     const action3 = setCartUsers(arrc);
-  //     dispatch(action3);
-  //     console.log(state.loginDetails);
-  //     navigate("/");
-  //   }
-  // };
+  let departmentId;
+
+
+  const changeFirstName = (e) => {
+    setFirstName(e.target.value);
+    console.log(firstName)
+  };
+  const changeLastName = (e) => {
+    setLastName( e.target.value);
+    console.log(lastName)
+  };
+  const changePhoneNumber = (e) => {
+    setPhoneNumber(e.target.value)
+    console.log(phoneNumber)
+  };
+  const changeAge = (e) => {
+    setAge(e.target.value)
+  };
+  const changePassword = (e) => {
+     setPassword( e.target.value)
+    console.log(password)
+  };
+
+  const bloodTypeChange = (e) => {
+    setBloodType( e.target.value)
+    console.log(bloodType)
+  };
+
+  
+  const byCategory = (e) => {
+    setCategory( e.target.value)
+    
+    console.log(category)
+
+  };
+
+  const genderChange = (e) => {
+    setGender(e.target.value)
+    console.log(gender)
+
+  };
+
+
+  const departmentChange = (e) => {
+     setDepartment(e.target.value)
+    if(department ==="Ophthalmology"){
+      departmentId=1
+    }
+    if(department ==="Pediatrics"){
+      departmentId=2
+    }
+    if(department ==="Dentists"){
+      departmentId=3
+    }
+    if(department ==="Internal medicine "){
+      departmentId=4
+    }
+    if(department ==="Ophthalmology "){
+      departmentId=5
+    }
+    console.log(department)
+
+  };
+
+  
+
+  
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/patients")
+      .then((res) => {
+        console.log(res.data);
+        setpatients(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    axios
+      .get("http://localhost:8080/doctors")
+      .then((res) => {
+        console.log(res.data);
+        setdoctors(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const getUser = () => {
+
+    const theuser = {
+      phonenumber: phoneNumber,
+      password: password,
+      
+    };
+    console.log("the user  want add ")
+    console.log(theuser)
+    let flag=0
+    let flag2=0
+    if(category === "Patient"){
+      patients.forEach((e) => {
+        if (e.phoneNumber === theuser.phonenumber) {
+           flag=1
+           console.log("i am in if i geted match")
+        }
+      });
+      if(flag===0){
+        axios
+        .post("http://localhost:8080/patients",{
+        firstName :""+firstName+"",
+        lasttName :""+lastName+"",
+        age:age,
+        bloodType:""+bloodType+"",
+        phoneNumber:""+phoneNumber+"",
+        password:""+password+"" ,
+        gender:gender})
+        .then((res) => {
+          console.log(res.data);
+                  alert("You have been successfully registered, you can log in now")
+        navigate("/Login");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
+      }
+      else if(flag===1){
+        alert("Sorry, the phone number is taken ")
+        
+      }
+      
+    }
+    else if(category === "Doctor"){
+      doctors.forEach((e) => {
+        if (e.phoneNumber === theuser.phonenumber) {
+           flag2=1
+           console.log("i am in if i geted match")
+        }
+      });
+      if(flag2===0){
+        axios
+        .post("http://localhost:8080/doctors",{
+          doctor:{
+            firstName :""+firstName+"",
+            lasttName :""+lastName+"",
+            gender:gender,
+            img: "/image/hernandezparra-jennifer.jpg",
+            phoneNumber:""+phoneNumber+"",
+            password:""+password+"" ,
+            },
+            departmentId:departmentId})
+        .then((res) => {
+          console.log(res.data);
+                  alert("You have been successfully registered, you can log in now")
+        navigate("/Login");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+        
+      }
+      else if(flag2===1){
+        alert("Sorry, the phone number is taken ")
+        
+      }
+    }}
+    
+  
+
+
 
   return (
     <div className="background">
@@ -106,7 +214,7 @@ function SignUp() {
               type="text"
               className="form-control"
               placeholder="First name"
-              // onChange={changeFirstName}
+              onChange={changeFirstName}
             />
           </div>
           <div className="form-group">
@@ -115,16 +223,16 @@ function SignUp() {
               type="text"
               className="form-control"
               placeholder="First name"
-              // onChange={changeLastName}
+              onChange={changeLastName}
             />
           </div>
           <div className="form-group">
-            <label>Email address </label>
+            <label>Age  </label>
             <input
-              type="email"
+              type="text"
               className="form-control"
-              placeholder="Enter email"
-              // onChange={changeEmail}
+              placeholder="Enter age"
+              onChange={changeAge}
             />
           </div>
           <div className="form-group">
@@ -133,7 +241,7 @@ function SignUp() {
               type="text"
               className="form-control"
               placeholder="Enter Phone number"
-              // onChange={changeUserName}
+              onChange={changePhoneNumber}
             />
           </div>
           <div className="form-group">
@@ -142,11 +250,86 @@ function SignUp() {
               type="password"
               className="form-control"
               placeholder="Enter password"
-              // onChange={changePassword}
+              onChange={changePassword}
             />
           </div>{" "}
-          <br />
-          <button type="submit" className="button-b">
+          <div className="form-group">
+
+
+            <label>Blood type</label>
+            {/* <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+              onChange={changePassword}
+            /> */}
+            <select onClick={bloodTypeChange} >
+              <option onSelect={bloodTypeChange}>A+</option>
+              <option onClick={bloodTypeChange}>A-</option>
+              <option onSelect={bloodTypeChange}>B+</option>
+              <option onSelect={bloodTypeChange}>B-</option>
+              <option onSelect={bloodTypeChange}>O-</option>
+              <option onSelect={bloodTypeChange}>O+</option>
+            </select>
+          </div>
+
+          <div className="form-group">
+          <label>
+            <input
+              type="radio"
+              onChange={genderChange}
+              name="gender"
+              value="Fmale"
+            />
+            Fmale
+          </label>
+          <label>
+            <input
+              type="radio"
+              onChange={genderChange}
+              name="gender"
+              value="Male"
+            />
+            Male
+          </label>
+          </div>
+
+          <div className="form-group">
+          <label>
+            <input
+              type="radio"
+              onChange={byCategory}
+              name="Category"
+              value="Patient"
+            />
+            Patient 
+          </label>
+          <label>
+            <input
+              type="radio"
+              onChange={byCategory}
+              name="Category"
+              value="Doctor"
+            />
+            Doctor
+          </label>
+          </div>
+          {
+            category === "Doctor" ?
+            <div className="form-group">
+            <label>Department</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter department"
+              onChange={departmentChange}
+            />
+          </div>
+            
+            :""
+          }
+                    <br />
+          <button type="button" onClick={() => { getUser(); }}  className="button-b">
             Sign Up
           </button>
           <p className="forgot-password text-right">
